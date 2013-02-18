dada39e56023fdd88dbdd77dd2545c7a4358ca16
haukurr11 <haukurr11@skel.ru.is>
2013-02-18 20:37:16 +0000

    modified:   tsh.c

--- tsh.c
+++ tsh.c
@@ -171,11 +171,35 @@ int main(int argc, char **argv)
  * background children don't receive SIGINT (SIGTSTP) from the kernel
  * when we type ctrl-c (ctrl-z) at the keyboard.  
 */
-void eval(char *cmdline) 
+void eval(char *cmdline)
 {
+    char *argv[MAXARGS];/* Argument list execve() */
+    char buf[MAXLINE]; /* Holds modified command line */
+    int bg; /* Should the job run in bg or fg? */
+    pid_t pid; /* Process id */
+    
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+    return; /* Ignore empty lines */
+    if (!builtin_command(argv)) {
+        if ((pid = Fork()) == 0) { /* Child runs user job */
+            if (execve(argv[0], argv, environ) &lt; 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+        /* Parent waits for foreground job to terminate */
+        if (!bg) {
+            int status;
+            if (waitpid(pid, &status, 0) &lt; 0)
+            unix_error("waitfg: waitpid error");
+        }
+        else
+        printf("%d %s", pid, cmdline);
+    }
     return;
 }
-
 /* 
  * parseline - Parse the command line and build the argv array.
  * 
